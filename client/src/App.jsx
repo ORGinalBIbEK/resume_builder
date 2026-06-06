34a06ce802d60fb347e282b134dcbe9e427843af
ORGinalBIbEK <bibekmhrzn23@gmail.com>
2026-06-06 18:52:53 +0545

    home page connection

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
-
+import {Route, Routes} from 'react-router-dom'
+import Home from './pages/Home.jsx'
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
+    </>
   )
 }
 
