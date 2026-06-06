47b801860bae1eb4b5bab44968e399e9d122c365
ORGinalBIbEK <bibekmhrzn23@gmail.com>
2026-06-06 20:23:27 +0545

    corrected the page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,17 +2,18 @@ import React from 'react'
 import {Route, Routes} from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import Layout from './pages/Layout.jsx'
+import Dashboard from './pages/Dashboard.jsx'
 import ResumeBuilder from './pages/ResumeBuilder.jsx'
 import Preview from './pages/Preview.jsx'
 import Login from './pages/Login.jsx'
 
+
 const App = () => {
   return (
-    <>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='app' element={<Layout/>}>
-        //subroute    
+
           <Route index element={<Dashboard/>}/>
           <Route path='builder/:resumeId' element={<ResumeBuilder/>}/>
         </Route>
@@ -20,7 +21,6 @@ const App = () => {
         <Route path='view/:resumeId' element={<Preview/>}/>
         <Route path='login' element={<Login/>}/>
       </Routes>
-    </>
   )
 }
 
