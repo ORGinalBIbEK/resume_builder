03a73a05e78c8fdd8cad9f600248b7dfaa2d3dcb
ORGinalBIbEK <bibekmhrzn23@gmail.com>
2026-06-06 20:16:57 +0545

    added 4 pages link

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import {Route, Routes} from 'react-router-dom'
 import Home from './pages/Home.jsx'
+import Layout from './pages/Layout.jsx'
+import ResumeBuilder from './pages/ResumeBuilder.jsx'
+import Preview from './pages/Preview.jsx'
+import Login from './pages/Login.jsx'
+
 const App = () => {
   return (
     <>
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='app' element={<Layout/>}>
+        //subroute    
+          <Route index element={<Dashboard/>}/>
+          <Route path='builder/:resumeId' element={<ResumeBuilder/>}/>
+        </Route>
+
+        <Route path='view/:resumeId' element={<Preview/>}/>
+        <Route path='login' element={<Login/>}/>
       </Routes>
     </>
   )
