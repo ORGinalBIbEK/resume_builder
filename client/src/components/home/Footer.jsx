2a3bb4fefd3f5ac2817160c73be0a04795bd0526
ORGinalBIbEK <bibekmhrzn23@gmail.com>
2026-06-15 21:09:11 +0545

    changed some setting and color

--- client/src/components/home/Footer.jsx
+++ client/src/components/home/Footer.jsx
@@ -8,29 +8,29 @@ const Footer = () => {
                     <a href="#">
                         <img src={logo} alt="logo" className="h-11 w-auto" />                    </a> 
                     <div>
-                        <p className="text-slate-100 font-semibold">Product</p>
+                        <p className="text-slate-800 font-semibold">Product</p>
                         <ul className="mt-2 space-y-2">
-                            <li><a href="/" className="hover:text-indigo-600 transition">Home</a></li>
-                            <li><a href="/" className="hover:text-indigo-600 transition">Support</a></li>
-                            <li><a href="/" className="hover:text-indigo-600 transition">Pricing</a></li>
-                            <li><a href="/" className="hover:text-indigo-600 transition">Affiliate</a></li>
+                            <li><a href="/" className="hover:text-green-600 transition">Home</a></li>
+                            <li><a href="/" className="hover:text-green-600 transition">Support</a></li>
+                            <li><a href="/" className="hover:text-green-600 transition">Pricing</a></li>
+                            <li><a href="/" className="hover:text-green-600 transition">Affiliate</a></li>
                         </ul>
                     </div>
                     <div>
-                        <p className="text-slate-100 font-semibold">Resources</p>
+                        <p className="text-slate-800 font-semibold">Resources</p>
                         <ul className="mt-2 space-y-2">
-                            <li><a href="/" className="hover:text-indigo-600 transition">Company</a></li>
-                            <li><a href="/" className="hover:text-indigo-600 transition">Blogs</a></li>
-                            <li><a href="/" className="hover:text-indigo-600 transition">Community</a></li>
-                            <li><a href="/" className="hover:text-indigo-600 transition">Careers<span className="text-xs text-white bg-indigo-600 rounded-md ml-2 px-2 py-1">We’re hiring!</span></a></li>
-                            <li><a href="/" className="hover:text-indigo-600 transition">About</a></li>
+                            <li><a href="/" className="hover:text-green-600 transition">Company</a></li>
+                            <li><a href="/" className="hover:text-green-600 transition">Blogs</a></li>
+                            <li><a href="/" className="hover:text-green-600 transition">Community</a></li>
+                            <li><a href="/" className="hover:text-green-600 transition">Careers<span className="text-xs text-white bg-green-600 rounded-md ml-2 px-2 py-1">We’re hiring!</span></a></li>
+                            <li><a href="/" className="hover:text-green-600 transition">About</a></li>
                         </ul>
                     </div>
                     <div>
-                        <p className="text-slate-100 font-semibold">Legal</p>
+                        <p className="text-slate-800 font-semibold">Legal</p>
                         <ul className="mt-2 space-y-2">
-                            <li><a href="/" className="hover:text-indigo-600 transition">Privacy</a></li>
-                            <li><a href="/" className="hover:text-indigo-600 transition">Terms</a></li>
+                            <li><a href="/" className="hover:text-green-600 transition">Privacy</a></li>
+                            <li><a href="/" className="hover:text-green-600 transition">Terms</a></li>
                         </ul>
                     </div>
                 </div>
@@ -38,7 +38,7 @@ const Footer = () => {
                     <p className="max-w-60">Making every customer feel valued—no matter the size of your audience.</p>
                     <div className="flex items-center gap-4 mt-3">
                         <a href="https://dribbble.com/prebuiltui" target="_blank" rel="noreferrer">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-dribbble size-5 hover:text-indigo-500" aria-hidden="true">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-dribbble size-5 hover:text-green-500" aria-hidden="true">
                                 <circle cx="12" cy="12" r="10"></circle>
                                 <path d="M19.13 5.09C15.22 9.14 10 10.44 2.25 10.94"></path>
                                 <path d="M21.75 12.84c-6.62-1.41-12.14 1-16.38 6.32"></path>
