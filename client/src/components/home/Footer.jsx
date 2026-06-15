9b928e2f42599e88f22a65a78c6de62f52b6dd8d
ORGinalBIbEK <bibekmhrzn23@gmail.com>
2026-06-15 21:41:33 +0545

    editing logo

--- client/src/components/home/Footer.jsx
+++ client/src/components/home/Footer.jsx
@@ -52,7 +52,7 @@ const Footer = () => {
                                 <circle cx="4" cy="4" r="2"></circle>
                             </svg>
                         </a>
-                        <a href="https://x.com/prebuiltui" target="_blank" rel="noreferrer">
+                        <a href="https://www.facebook.com/bibek.maharjan.353/" target="_blank" rel="noreferrer">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-twitter size-5 hover:text-green-500" aria-hidden="true">
                                 <path
                                     d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z">
