a3707ba8e82863f61bebb527df92124e9eac0199
ORGinalBIbEK <bibekmhrzn23@gmail.com>
2026-06-18 21:11:59 +0545

    setting up

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 const Navbar = () => {
+    const user={name:'Bibek Maharjan'}
   return (
-    <div>
-      
+    <div className='shadow bg-white'>
+        <nav className='flex items-center justify-between max-w-7xl mx-auto px-4 py-3.5 text-slate-800 transition-all
+        '></nav>
+      <Link 
     </div>
   )
 }
