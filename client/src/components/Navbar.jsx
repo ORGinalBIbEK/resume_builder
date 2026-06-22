80eaac5ac15ca86ee331049dfdf13385d5dced5e
ORGinalBIbEK <bibekmhrzn23@gmail.com>
2026-06-22 14:11:01 +0545

    button in navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,7 +6,13 @@ const Navbar = () => {
     <div className='shadow bg-white'>
         <nav className='flex items-center justify-between max-w-7xl mx-auto px-4 py-3.5 text-slate-800 transition-all
         '></nav>
-      <Link 
+      <Link to='/'>
+        <img src="/logo.svg" alt='logo' className='h-11 w-auto'/>
+      </Link>
+      <div>
+        <P>Hi,{user?.name}</P>
+        <button></button>
+      </div>
     </div>
   )
 }
