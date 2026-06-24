19cfcd0b928515547621712f8dfd98e530095ada
ORGinalBIbEK <bibekmhrzn23@gmail.com>
2026-06-24 22:55:30 +0545

    bg color in btton tag

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -44,7 +44,8 @@ const Dashboard = () => {
             const baseColor=color[index% VideoColorSpace.length];
             return (
               <button key={index} className='relative w-full sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg gap-2 border group hover:shadow-lg
-              transition-all duration-300 cursor-pointer'></button>
+              transition-all duration-300 cursor-pointer' style={{
+                background:`linear-gradient(135deg,${baseColor}10),${baseColor}40`}}></button>
             )
           })}
 
