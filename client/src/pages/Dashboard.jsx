939bd2186fdc9490abf0c3d0c0c9214db497830d
ORGinalBIbEK <bibekmhrzn23@gmail.com>
2026-06-25 21:04:33 +0545

    css in the new button

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -82,7 +82,7 @@ const Dashboard = () => {
             <div>
               <h2>Create a Resume</h2>
               <input type='text' placeholder='Enter resume title' className='w-full px-4 py-2 mb-4 focus:border-green-600'required/>
-              <button>
+              <button className='w-full py-2 bg-green-600 text-white rounded hover:bg-green-700 transition-colors'>Create Resume
                 </button>            
             </div>
 
